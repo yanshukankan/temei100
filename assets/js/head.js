563619b98ddf43524a4ee3f28f8bb3889cde6763
lijiaocn <lijiaocn@foxmail.com>
2019-05-25 20:20:33 +0800

    m

--- assets/js/head.js
+++ assets/js/head.js
@@ -8,27 +8,15 @@ enable_page_level_ads: true
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-105389858-1');
+gtag('config', 'UA-140817786-1');
 
 /*百度统计*/
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8f59ea10145df28da8932c8743ec1d60";
+  hm.src = "https://hm.baidu.com/hm.js?0c08ebf10e4f28c83514963b7a4479ee";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
 
 /*百度自动收录*/
-(function(){
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-})();
